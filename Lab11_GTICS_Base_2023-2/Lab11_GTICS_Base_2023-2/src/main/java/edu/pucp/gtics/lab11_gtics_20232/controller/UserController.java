8fe7b56d15bbe89bbf2da067a8b7ae0e15fa74d2
RommelGaray <a20200241@pucp.edu.pe>
2023-11-05 11:57:53 -0500

    ultimo

--- Lab11_GTICS_Base_2023-2/Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/controller/UserController.java
+++ Lab11_GTICS_Base_2023-2/Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/controller/UserController.java
@@ -1,21 +1,38 @@
 package edu.pucp.gtics.lab11_gtics_20232.controller;
 
 
+import edu.pucp.gtics.lab11_gtics_20232.entity.User;
 import edu.pucp.gtics.lab11_gtics_20232.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
     @Autowired
     UserRepository userRepository;
+
+    final PasswordEncoder passwordEncoder;
+
+    public UserController() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+
 //    @GetMapping(value = {"/",""})
 //    public String principal(){
 //        return "juegos/lista";
 //    }
-    @GetMapping(value = {"/openLoginWindow","","/"})
+    @GetMapping(value = {"/openLoginWindow","","/","/login"})
     public String loginWindow(){
         System.out.println(userRepository.findAll());
         return "user/signIn";
@@ -23,6 +40,22 @@ public class UserController {
     @GetMapping("/closeSession")
     public String closeSession(){
         return "user/signUp";
+    }
+
+    @PostMapping(value = "/guardarUser")
+    public String guardarUsuario(@ModelAttribute("usuario") @Valid User user, BindingResult bindingResult, RedirectAttributes attr){
+        if(bindingResult.hasErrors()){
+            return "user/signUp";
+        } else {
+            user.setEnable(1);
+            user.setAutorizacion("USER");
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+            attr.addFlashAttribute("msg","Usuario registrado");
+            userRepository.save(user);
+
+            return "redirect:/login";
+        }
     }
 
 }
